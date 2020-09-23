bdf955c95062b704d039b054a6fdb604bab6fd90
Aorosee <xiang.a@outlook.com>
2020-09-23 11:19:59 -0400

    commit on master

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h2>
+          This is new work about Next.js
+        </h2>
+
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
